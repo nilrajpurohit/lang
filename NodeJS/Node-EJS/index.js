f2e76faf202a6dd147706aaf4874fe9a97769091
nilrajpurohit <Nilrajp1318@gmail.com>
2022-02-24 16:34:48 +0530

    Node_EJS first commit

---
+++ NodeJS/Node-EJS/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+require('dotenv').config()
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.static('public/assets/css'));
+app.use(express.static('public/assets/js'));
+app.use(express.static('public/image'));
+//register engine
+app.set('view engine', 'ejs');
+
+app.use(morgan('combined'));
+
+app.get(['/', '/home'], (req, res) => {
+    var blogs = [{
+            title: 'Blog 1',
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            title: 'Blog 2',
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            title: 'Blog 3',
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            title: 'Blog 4',
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+    ]
+
+    res.render('pages/home', {
+        title: 'Home',
+        blogs: blogs
+    });
+})
+
+app.use((req, res) => {
+    res.render('error', {
+        title: '404',
+    });
+})
+
+
+app.listen('3000', () => {
+    console.log('http://localhost:3000');
+});
